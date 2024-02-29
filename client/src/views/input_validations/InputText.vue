ed3a09c8508b9f3327ad5c562818c932e93e4f10
Cristian Campos <20213tn003@utez.edu.mx>
2024-02-29 12:59:04 -0600

    [ADD] resolve problems

--- client/src/views/input_validations/InputText.vue
+++ client/src/views/input_validations/InputText.vue
@@ -2,12 +2,13 @@
   <div>
     <b-row>
       <b-col md="3">
-
-
-        <b-card header="Información sobre la validación" header-text-variant="primary">
+        <b-card
+          header="Información sobre la validación"
+          header-text-variant="primary"
+        >
           <ul class="px-3">
             <li>El valor del campo es requerido.</li>
-            <li>Permite maayúsculas y minúsculas.</li>
+            <li>Permite mayúsculas y minúsculas.</li>
             <li>Permite espacios en blanco entre las palabra</li>
             <li>Permite guiones medios o bajos</li>
             <li>Permite acentos</li>
@@ -15,37 +16,40 @@
           </ul>
         </b-card>
 
-
         <b-card header="Función" header-text-variant="primary" class="mt-2">
           <ul class="px-3">
-            <li>Si el campo se toca y no se ha escrito nada, se comenzará a validar.</li>
+            <li>
+              Si el campo se toca y no se ha escrito nada, se comenzará a
+              validar.
+            </li>
           </ul>
         </b-card>
-
       </b-col>
       <b-col>
         <b-card>
           <b-form @submit.prevent="onSubmit">
-
             <b-form-group>
               <label for="name">Nombre:</label>
               <b-form-input
-                  id="name"
-                  type="text"
-                  placeholder="Juan"
-                  required
-                  v-model="v$.form.name.$model"
-                  @blur="v$.form.name.$touch()"
-                  :state="v$.form.name.$dirty ? !$v.form.name.$error : null"
-
+                id="name"
+                type="text"
+                placeholder="Juan"
+                required
+                v-model="v$.form.name.$model"
+                @blur="v$.form.name.$touch()"
+                :state="v$.form.name.$dirty ? !v$.form.name.$error : null"
               />
-
-
-              {{
-                v$.form
-              }}
+              <h5 class="mt-3">Objeto de vuelidate</h5>
+              {{ v$.form }}
+              <h5 class="mt-3">Errores del campo</h5>
+              <p v-for="error of v$.$errors" :key="error.$uid">
+                <strong>{{ error.$validator }}</strong>
+                <small> on property </small>
+                <strong>{{ error.$property }}</strong>
+                <small> says: </small>
+                <strong>{{ error.$message }}</strong>
+              </p>
             </b-form-group>
-
 
             <!--    message to simulate the sending of the form    -->
             <div v-if="simulationShow">
@@ -61,7 +65,13 @@
             </div>
 
             <div class="text-right">
-              <b-button type="reset" variant="danger" class="mx-3" @click="onReset">Limpiar</b-button>
+              <b-button
+                type="reset"
+                variant="danger"
+                class="mx-3"
+                @click="onReset"
+                >Limpiar</b-button
+              >
               <b-button type="submit" variant="primary">Enviar</b-button>
             </div>
           </b-form>
@@ -73,54 +83,44 @@
 
 <script>
 import Vue from "vue";
-import {useVuelidate} from '@vuelidate/core'
-import {required} from '@vuelidate/validators'
+import { useVuelidate } from "@vuelidate/core";
+import { required, alfa } from "@vuelidate/validators";
 
 export default Vue.extend({
   name: "InputText",
   setup() {
-    return {v$: useVuelidate()}
+    return { v$: useVuelidate() };
   },
   data() {
     return {
       form: {
-        name: ""
-      } ,
+        name: "",
+      },
       simulationShow: false,
       simulationSend: false,
-
     };
   },
   methods: {
     onSubmit() {
-
-
-
-
-      this.simulationShow = true
-      this.simulationSend = false
+      this.simulationShow = true;
+      this.simulationSend = false;
       setTimeout(() => {
-        this.simulationSend = true
-      }, 1000)
+        this.simulationSend = true;
+      }, 1000);
       setTimeout(() => {
-        this.simulationShow = false
-      }, 2000)
+        this.simulationShow = false;
+      }, 2000);
     },
     onReset() {
-      this.v$.$reset()
-    }
+      this.v$.$reset();
+    },
   },
-  validations() {
-    return {
-      form: {
-        name: {required}
-      }
-    }
-  }
-})
+  validations: {
+    form: {
+      name: { required, alfa },
+    },
+  },
+});
 </script>
 
-
-<style scoped>
-
-</style>
+<style scoped></style>
